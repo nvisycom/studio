d94d0540a0ac6f02df42511939bd01ea91ec6b6a
Oleh Martsokha <o.martsokha@gmail.com>
2025-10-17 06:10:26 +0200

    ui lib

---
+++ packages/desktop/i18n/config.ts
@@ -0,0 +1,9 @@
+// import { pluralRule } from "./";
+
+// https://i18n.nuxtjs.org/docs/composables/define-i18n-config
+export default defineI18nConfig(() => ({
+	pluralRules: {
+		// lang: pluralRule,
+		// lang: pluralRule,
+	},
+}));
--- packages/desktop/nuxt.config.ts
+++ packages/desktop/nuxt.config.ts
@@ -1,8 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
+	telemetry: { enabled: false },
 	ssr: false,
+
+	css: ["~/assets/fonts.css", "~/assets/tailwind.css"],
+	modules: [
+		"@nuxt/devtools",
+		"@nuxt/image",
+		"@nuxtjs/i18n",
+		"@nuxtjs/color-mode",
+		"@nuxtjs/device",
+	],
 
 	devServer: {
 		// Enables the development server to be discoverable by other devices
@@ -20,8 +32,36 @@ export default defineNuxtConfig({
 			// Tauri requires a consistent port
 			strictPort: true,
 		},
+		plugins: [tailwindcss()],
 	},
 
 	// Avoids [unhandledRejection] EMFILE: too many open files
 	ignore: ["**/src-tauri/**"],
+
+	colorMode: {
+		classSuffix: "",
+		storage: "cookie",
+		preference: "system",
+		fallback: "light",
+	},
+
+	i18n: {
+		strategy: "no_prefix",
+		detectBrowserLanguage: {
+			useCookie: true,
+			cookieKey: "i18n_redirected",
+			redirectOn: "root",
+		},
+
+		locales: [
+			{ code: "en", name: "English", file: "en.json" },
+			{ code: "de", name: "Deutsch", file: "de.json" },
+		],
+		defaultLocale: "en",
+
+		compilation: {
+			strictMessage: true,
+			escapeHtml: true,
+		},
+	},
 });
