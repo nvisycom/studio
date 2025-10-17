09233b040b43b5256ceaf1d01d06c156a36766ad
Oleh Martsokha <o.martsokha@gmail.com>
2025-10-17 09:32:58 +0200

    abc

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,2 +1,13 @@
 // Components
 export { default as OpenStatus } from "./OpenStatus.vue";
+
+// UI Components
+export * from "./ui/button";
+export * from "./ui/card";
+export * from "./ui/input";
+export * from "./ui/textarea";
+export * from "./ui/label";
+export * from "./ui/checkbox";
+export * from "./ui/select";
+export * from "./ui/separator";
+export * from "./ui/badge";
